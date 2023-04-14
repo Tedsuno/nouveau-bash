47111bcf4c37a20848748640650d5d04dd2055e9
Amine Sekkat <aminesekkat593@gmail.com>
2023-04-14 23:40:28 +0200

    ajout de la nouvelle class

--- nouveauBash.c
+++ nouveauBash.c
@@ -20,14 +20,6 @@ struct liste_noeud * succ ;
 typedef struct noeud noeud ;
 typedef struct liste_noeud liste_noeud ;
 /*-------------------------------------------*/
-void ls(noeud* courant){
-	liste_noeud* fils=courant->fils;
-	while(fils!=NULL){
-		printf("%s\n",fils->no->nom);
-		fils=fils->succ;
-	}
-}
-/*-------------------------------------------*/
 char* capture(const char* nom) {
     char* result = malloc(strlen(nom) + 1); // Allouer de la mémoire pour la chaîne de caractères de retour
     if (result != NULL) { // Vérifier si l'allocation a réussi
@@ -79,6 +71,59 @@ while(liste!=NULL){
 free(c);
 return false;
 }
+/*-------------------------------------------*/
+void ls(noeud* courant){
+	liste_noeud* fils=courant->fils;
+	while(fils!=NULL){
+		printf("%s\n",fils->no->nom);
+		fils=fils->succ;
+	}
+}
+/*-------------------------------------------*/
+noeud* cd_chem(noeud* courant, const char* chem){
+	//Si c'est un chemin absolu
+    char* c=capture(chem);
+    if(c[0])
+	/*+++++++++++++++++++++++++++++++++++++++*/
+
+	//Si ce n'est pas un chemin absolu
+}
+/*-------------------------------------------*/
+noeud* cd(noeud* courant){
+    while(courant->pere != courant->racine){
+        courant=courant->pere;
+    }
+    return courant;
+}
+/*-------------------------------------------*/
+noeud* cd_point(noeud* courant){
+    if(courant->pere==NULL){ perror("No such file or directory");}
+    if(courant->pere!=NULL){ return courant->pere;}
+    return NULL;
+}
+/*-------------------------------------------*/
+void pwd(noeud* courant){
+    noeud* c = courant;
+    char path[1000] = "";
+    char chaine[1000];
+    char chaine2[99];
+    if (strlen(c->nom) > 0) {
+        strcat(path, "/");
+        strcat(path, c->nom);
+    }
+    strcpy(chaine, path);
+    while(c->pere != c->racine){
+        c = c->pere;
+        strcpy(chaine2, chaine); // chaine2 = /TD[1]
+        strcpy(chaine, "");      // chaine = "" 
+        if (strlen(c->nom) > 0) {
+            strcat(chaine, "/");
+            strcat(chaine, c->nom); // chaine = /anglais
+        }
+        strcat(chaine, chaine2); // chaine = /anglais/TD[1]
+    }
+    printf("%s\n", chaine);
+}
 /*---------------------------------------------------------------*/
 void mkdir(noeud* parent, const char *nom) {
     if(estValide(nom) && !existeDeja(parent,nom)){
@@ -128,6 +173,32 @@ free(c);
 else{
 	perror("Le nom est invalide ou un fichier portant ce nom existe déjà dans ce dossier.");
 }
+}
+/*---------------------------------------------------------------*/
+int nb_fils(noeud* courant) {
+    if(courant->fils==NULL) return 0;
+    int count=0;
+    liste_noeud* fils=courant->fils;
+    while (fils!=NULL) {
+        count++;
+        fils=fils->succ;
+    }
+      return count;
+}
+/*---------------------------------------------------------------*/
+void print_arbre(noeud* courant) {
+    printf("Noeud %s (%s), %d fils : ", courant->nom, courant->est_dossier ? "D" : "F", nb_fils(courant));
+    if (courant->fils == NULL) {
+        printf("Aucun fils\n");
+        return;
+    }
+    puts("");
+    liste_noeud *fils=courant->fils;
+    while (fils!=NULL) {
+        printf("Noeud %s (%s), pere : %s, ", fils->no->nom, fils->no->est_dossier ? "D" : "F", courant->nom);
+        print_arbre(fils->no);
+        fils=fils->succ;
+    }
 }
 /*-------------------------------------------*/
 int main(void){
