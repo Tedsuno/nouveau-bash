58d2af0c40e7eee28f1975866deabbd8149b2d4f
Amine Sekkat <aminesekkat593@gmail.com>
2023-05-19 01:35:15 +0200

    je

--- src/main.c
+++ src/main.c
@@ -5,123 +5,165 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+
+bool is_commande(char* ch){
+    if(ch!=NULL){
+    if(strcmp(ch,"ls")!=0 && strcmp(ch,"pwd")!=0 && strcmp(ch,"mkdir")!=0 && strcmp(ch,"cp")!=0 && strcmp(ch,"rm")!=0
+        && strcmp(ch,"cd")!=0 && strcmp(ch,"mv")!=0 && strcmp(ch,"touch")!=0 && strcmp(ch,"print")!=0 )
+        return false;
+    }
+    return true; 
+}
+
+char* commande_terminale(char* phrase, int count) {
+    char* resultat = strdup(phrase);
+    char* token = NULL;
+    
+    for (int i = 0; i < count; i++) {
+        if (resultat != NULL) {
+            token = strtok(resultat, " \n");
+            resultat = strtok(NULL, "");
+        } else {
+            return NULL;  // Renvoie NULL si le nombre de mots est dépassé
+        }
+    }
+    
+    if (token != NULL) {
+        return strdup(token);
+    } else {
+        return NULL;
+    }
+}
+
 /*--------------------------------------------------------*/
 int main(int argc, char *argv[]){
+
     noeud* racine=creerRacine();
     noeud* courant=racine;
+
     if (argc != 2) {
         printf("Usage: %s fichier\n", argv[0]);
         free(racine);
         return 1;
     }
-   char ch[100];
-   char temp[100];
-   char *token[100];
-   char *token2;
-   char *token3[200];
-   FILE* flux = fopen(argv[1], "r");
+
+    char ch[100];
+    FILE* flux = fopen(argv[1], "r");
+
     if (flux == NULL) {
     perror("Probleme ouverture de fichier");
-   } else {
-    int c = 0;
-    while (fgets(ch, 50, flux) != NULL) {
-        strcpy(temp, ch);
-        token[c] = strtok(temp, " ");
-        token3[c] = temp;
-        token2=token[c]; 
-        token2 = strchr(ch, ' '); 
-        if (token2 != NULL) {   
-        token2++;
-        int len=strlen(token2);
-        if (len > 0) {
-        if (token2[len - 1] == '\n') {
-                    token2[len - 1] = '\0';
-                }
-            }        
-        }
-    
-    if(strcmp(token[c],"mkdir")==0){
-            mkdir(courant,token2);
     }
-     if(strcmp(token[c],"print")==0 || strcmp(token[c],"print\n")==0){
-            char *token_bis = strtok(token2, " ");
-            if(token_bis!=NULL && strlen(token_bis)!=0){
-                perror("error noms invalides pour print");
-                exit(EXIT_FAILURE);
-            }
+
+    while (fgets(ch, 50, flux) != NULL) {
+
+    if(!is_commande(commande_terminale(ch,1))){
+        printf("Vous avez entré une commande inconnue : %s",commande_terminale(ch,1));
+        free_chem(racine);
+        free(racine);
+        exit(EXIT_FAILURE);
+    }
+    if(commande_terminale(ch,1)!=NULL){
+
+    if(strcmp(commande_terminale(ch,1),"mkdir")==0){
+        if(commande_terminale(ch,2)!=NULL && commande_terminale(ch,3)==NULL){
+            mkdir(courant,commande_terminale(ch,2));
+        }
+        else{
+            perror("erreur mkdir, ne respecte pas le format de base."); free_chem(racine);
+            free(racine); exit(EXIT_FAILURE);  
+        }
+    }
+     if(strcmp(commande_terminale(ch,1),"print")==0){
+        if(commande_terminale(ch,2)==NULL){
+            printf("-----print-----\n");
             noeud* bis=courant;
             courant=courant->racine;
             print_arbre(courant);
             courant=bis;
-            puts("-------------");
+            printf("---------------\n");
+        }
+        else{
+            perror("erreur print, ne respecte pas le format de base."); free_chem(racine);
+            free(racine); exit(EXIT_FAILURE);  
+        }
     }
-    if(strcmp(token[c],"mv")==0){
-           char *str1 = NULL;
-           char *str2 = NULL;
-           char *token_bis = strtok(token2, " ");
-           if (token_bis != NULL) {
-              str1 = strdup(token_bis);
-              token_bis = strtok(NULL, " ");
-              str2 = strdup(token_bis);
-              token_bis = strtok(NULL, " ");
-              if(token_bis!=NULL && strlen(token_bis)!=0){
-                perror("error noms invalides pour mv");
-                exit(EXIT_FAILURE);
-              }
-           }
-           mv(courant,str1,str2);
+    if(strcmp(commande_terminale(ch,1),"mv")==0){
+        if(commande_terminale(ch,2)!=NULL && commande_terminale(ch,3)!=NULL && commande_terminale(ch,4)==NULL) mv(courant,commande_terminale(ch,2),commande_terminale(ch,3));
+        else{ perror("erreur, mv ne respecte pas le format de base."); free_chem(racine);
+            free(racine); exit(EXIT_FAILURE); 
+        }
     }
-    if(strcmp(token[c],"cp")==0){
-    	   char *str1 = NULL;
-           char *str2 = NULL;
-           char *token_bis = strtok(token2, " ");
-           if (token_bis != NULL) {
-              str1 = strdup(token_bis);
-              token_bis = strtok(NULL, " ");
 
-              str2 = strdup(token_bis);
-              token_bis = strtok(NULL, " ");
-              if(token_bis!=NULL && strlen(token_bis)!=0){
-                perror("error noms invalides pour cp");
-                exit(EXIT_FAILURE);
-              }
-           }
-           cp(courant,str1,str2);
-    }    
-    if(strcmp(token3[c],"ls")==0){
+    if(strcmp(commande_terminale(ch,1),"cp")==0){
+        if(commande_terminale(ch,2)!=NULL && commande_terminale(ch,3)!=NULL && commande_terminale(ch,4)==NULL) cp(courant,commande_terminale(ch,2),commande_terminale(ch,3));
+        else{ perror("erreur, cp ne respecte pas le format de base."); free_chem(racine);
+            free(racine); exit(EXIT_FAILURE); 
+        }
+    } 
+
+    if(strcmp(commande_terminale(ch,1),"ls")==0){
+        if(commande_terminale(ch,2)==NULL){
+            printf("-----ls-----\n");
             ls(courant);
-            puts("-------------");
+            printf("---------------\n");
+        }
+        else{
+            perror("erreur ls, ne respecte pas le format de base."); free_chem(racine);
+            free(racine); exit(EXIT_FAILURE);  
+        }
     }
-    if(strcmp(token3[c],"rm")==0){
-            rm(courant,token2);
+    if(strcmp(commande_terminale(ch,1),"rm")==0){
+        if(commande_terminale(ch,2)!=NULL && commande_terminale(ch,3)==NULL){
+            rm(courant,commande_terminale(ch,2));
+        }
+        else{
+            perror("erreur rm, ne respecte pas le format de base."); free_chem(racine);
+            free(racine); exit(EXIT_FAILURE);  
+        }
    }
-    if(strcmp(token3[c],"pwd")==0 || strcmp(token3[c],"pwd\n")==0){
+
+    if(strcmp(commande_terminale(ch,1),"pwd")==0){
+        if(commande_terminale(ch,2)==NULL){
+            printf("------pwd------\n");
             pwd(courant);
-            puts("-------------");
+            printf("---------------\n");
+        }
+        else{
+            printf("%s",commande_terminale(ch,1));
+            perror("erreur pwd, ne respecte pas le format de base."); free_chem(racine);
+            free(racine); exit(EXIT_FAILURE);  
+        }
     }
-    if(strcmp(token3[c],"touch")==0){
-            touch(courant,token2);
-    }    
-    if(strcmp(token3[c], "cd\n") == 0)courant=cd(courant);
-    if(strcmp(token3[c],"ls\n")==0){ ls(courant); puts("-------------"); }
-    if(strcmp(token3[c],"cd")==0 && token2!=NULL){ 
-        //printf("aaaa");   
-        if(strcmp(token2,"..")==0 || strcmp(token2,"..\n")==0){ courant=cd_point(courant); }
-        else { courant=cd_chem(courant,token2); }
+    if(strcmp(commande_terminale(ch,1),"touch")==0){
+        if(commande_terminale(ch,2)!=NULL && commande_terminale(ch,3)==NULL){
+        touch(courant,commande_terminale(ch,2));
+        }
+        else{
+            perror("erreur touch, ne respecte pas le format de base."); free_chem(racine);
+            free(racine); exit(EXIT_FAILURE);  
+        }
     }
-    else if((strcmp(token3[c],"\n")!=0 && strcmp(token3[c],"cd")!=0 && strcmp(token3[c],"touch")!=0 && strcmp(token3[c],"pwd")!=0 && strcmp(token3[c],"pwd\n")!=0 && 
-            strcmp(token3[c],"rm")!=0 && strcmp(token3[c],"ls")!=0 && strcmp(token3[c],"cp")!=0
-            && strcmp(token3[c],"mv")!=0 && strcmp(token3[c],"mkdir")!=0 && strcmp(token3[c],"print")!=0 && strcmp(token3[c],"print\n")!=0
-             && strcmp(token3[c],"cd\n")!=0 && strcmp(token3[c],"ls\n")!=0)){
-        printf("command not found :%s",token3[c]);
-        exit(EXIT_FAILURE); 
+
+    if(strcmp(commande_terminale(ch,1), "cd") == 0){
+        if(commande_terminale(ch,2)==NULL){
+            courant=cd(courant);
+        }
+        else if(commande_terminale(ch,3)==NULL){
+            if(strcmp(commande_terminale(ch,2), "..") == 0) courant=cd_point(courant);
+            if(strcmp(commande_terminale(ch,2), "..") != 0) courant=cd_chem(courant,commande_terminale(ch,2));
+        } 
+        else{
+            perror("erreur cd, ne respecte pas le format de base.");
+            free_chem(racine);
+            free(racine);
+            exit(EXIT_FAILURE);
+        }  
     }
-    c++;  
-    }
+  }
+}
     int r = fclose(flux);
     if (r != 0) {
         perror("Probleme fermeture de fichier");
-    }
     }
     free_chem(racine);
     free(racine);
